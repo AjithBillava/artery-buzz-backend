1e3272336dd1eef624d638b0b2f09a7a1df5d1d3
Ajith <ajit.v.billava@gmail.com>
2021-10-07 20:33:23 +0530

    Update db.connect.js

--- db/db.connect.js
+++ db/db.connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const initialiseDatabaseConnection = async() =>{
     try{
-        await mongoose.connect(process.env.db_Path,{
+        await mongoose.connect(process.env.db_path,{
             useUnifiedTopology:true,
             useNewUrlParser:true 
         })
