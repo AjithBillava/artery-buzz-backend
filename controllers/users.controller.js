f112f8ae0b7c7d98310f4449c9651b199642db8f
Ajith <ajit.v.billava@gmail.com>
2021-10-12 21:15:14 +0530

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -153,13 +153,16 @@ const getCurrentUser = async(req,res,next) =>{
         populate({
             path:"posts",
             populate:{
-                path:"likedUsers",
-                select:"-__v -password",
-            },
-            populate:{
                 path:"author",
                 select:"-__v -password",
             }
+        }).
+        populate({
+            path:"posts",
+            populate:{
+                path:"likedUsers",
+                select:"-__v -password",
+            },
         })
         // const foundUser  = await User.findById(userId).populate("following").populate("followers").populate("posts")
 
